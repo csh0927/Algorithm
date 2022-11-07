b1c790dacba6363988a26cb8f7b6bec5cfdc649e
최성현 <103028391+csh0927@users.noreply.github.com>
2022-11-07 15:22:03 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.py
@@ -0,0 +1,11 @@
+n = int(input())
+nn = n - 1
+b = 1
+for i in range(1, n + 1, 1):
+    for j in range(nn, 0, -1):
+        print(" ", end='')
+    for j in range(1, b + 1, 1):
+        print("*", end='')
+    b += 2
+    nn -= 1
+    print()
