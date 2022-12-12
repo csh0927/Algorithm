dbae36978b0b79c5be7e9c8fe96a45997c0b59c1
최성현 <103028391+csh0927@users.noreply.github.com>
2022-12-12 18:40:25 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,8 @@
+k, kq, l, b, n, p = map(int, input().split())
+
+print(1 - k, end=' ')
+print(1 - kq, end=' ')
+print(2 - l, end=' ')
+print(2 - b, end=' ')
+print(2 - n, end=' ')
+print(8 - p, end=' ')
