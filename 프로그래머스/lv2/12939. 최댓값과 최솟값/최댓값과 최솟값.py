5e594a3903f5c831028437c6f057d848935abd9b
최성현 <103028391+csh0927@users.noreply.github.com>
2023-03-22 08:35:24 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.17 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = []
+    s = s.split()
+    
+    for i in s:
+        answer.append(int(i))
+        minmax = "{} {}".format(min(answer), max(answer))
+        
+    
+    return minmax
