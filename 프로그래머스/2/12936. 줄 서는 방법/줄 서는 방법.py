de73c90991e63c8840cf98e989800203da52888f
최성현 <103028391+csh0927@users.noreply.github.com>
2024-05-28 15:30:24 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,12 +1,12 @@
 import math
  
 def solution(n, k):
-    arr = [i for i in range(1, n + 1)]
+    nl = [i for i in range(1, n + 1)]
     answer = []
     
-    while arr:
-        a = (k - 1) // math.factorial(n - 1)
-        answer.append(arr.pop(a))
+    while nl:
+        i = (k - 1) // math.factorial(n - 1)
+        answer.append(nl.pop(i))
  
         k = k % math.factorial(n - 1)
         n -= 1
